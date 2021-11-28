2cb34f84284cb15b7c6114620071d324a223dd1c
ian <ian@ian.com>
2021-11-28 19:15:19 +0330

    Added React UI, Ads and Categories tables. AND added migrations, models and controllers.

---
+++ database/migrations/2021_11_28_140250_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('description');
+            $table->integer('price');
+            $table->longText('address');
+            $table->integer('phoneNumber');
+            $table->integer('userID');
+            $table->integer('categoryID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
