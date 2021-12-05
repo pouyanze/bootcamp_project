cad8ad2514d20caaa1048257e3ce0655dc30e63a
ian <ian@ian.com>
2021-12-06 00:03:18 +0330

    AdvertisementsController is a new name!

---
+++ app/Http/Controllers/AdvertisementsController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Advertisement;
+use App\Models\Category;
+
+class AdvertisementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function list()
+    {
+        $ads = Advertisement::where('userID', Auth::user()->id)->get();
+
+        return view('ads/list', ['ads' => $ads]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('ads/create', ['categories' => $categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $ad = Advertisement::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'price' => $request->input('price'),
+            'address' => $request->input('address'),
+            'phoneNumber' => $request->input('phoneNumber'),
+            'userID' => $request->input('userID'),
+            'categoryID' => $request->input('categoryID'),
+        ]);
+
+        return redirect('ads/list');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request, $id)
+    {
+        $ad = Advertisement::where('id', $id)->update([
+            'title' => $request->input('newAdTitle'),
+            'description' => $request->input('newAdDescription'),
+            'price' => $request->input('newAdAdPrice'),
+            'address' => $request->input('newAdAddress'),
+            'phoneNumber' => $request->input('newAdPhoneNumber'),
+        ]);
+        return redirect('ads/list');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $ad = Advertisement::where('id', $id)->delete();
+        return redirect('ads/list');
+    }
+}
