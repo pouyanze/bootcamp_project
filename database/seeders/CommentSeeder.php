f66dc8052a923f779eda1a8ecd8be9d49880d2b1
ian <ian@ian.com>
2021-12-08 01:15:33 +0330

    created comment seeder

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Nette\Utils\Random;
+use Illuminate\Support\Enumerable;
+use Illuminate\Support\LazyCollection;
+
+
+class CommentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $userrrIDDD = DB::table('users')->pluck('id')->toArray();
+        for ($x = 1; $x <= 3; $x++) {
+            DB::table('comments')->insert([
+                'id' => $x,
+                'title' => 'عنوان نظر: '.Str::random(2),
+                'description' => 'متن نظر: '.Str::random(10),
+                'userID' => array_pop($userrrIDDD),
+            ]);
+            }
+    }
+}
