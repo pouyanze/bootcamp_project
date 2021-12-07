2f5947a05cb3dc38fa2174af46c3b8a489398422
ian <ian@ian.com>
2021-12-08 00:58:13 +0330

    edited for bette visual

--- resources/views/ads/list.blade.php
+++ resources/views/ads/list.blade.php
@@ -1,48 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container text-center">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <h1>لیست آگهی ها</h1>
+                    <h1>آگهی های من</h1>
                 </div>
                 <div class="card-body">
-                    <table class="table">
-                        <tr style="display: inline;">
-                            <th scope="col">--آگهی--</th>
-                            <th scope="col">حذف</th>
+                    <table class="table table-bordered table-hover">
+                        <tr>
+                            <th scope="col">--آگهی--
+
+                            <th scope="col"> ویرایش</th>
+
+                            </th>
+                            <th scope="col">پاک کردن</th>
                         </tr>
 
                         @foreach ($ads as $ad)
-                        <tr style="display: inline;">
-                            <div class="mb-3">
+                        <tr>
+                            <td class="align-middle">
                                 <form action="{{$ad->id}}/edit" method="post">
-                                @csrf
-                                    <td>
-                                        <!-- <input type="hidden" name="adIDforCRUD" class="adID" value="{{$ad->id}}"> -->
-                                        <input type="text" name="newAdTitle" value="{{$ad->title}}" class="form-control">
-                                        <input type="text" name="newAdDescription" value="{{$ad->description}}" class="form-control">
-                                        <input type="text" name="newAdAdPrice" value="{{$ad->price}}" class="form-control">
-                                        <input type="text" name="newAdAddress" value="{{$ad->address}}" class="form-control">
-                                        <input type="text" name="newAdPhoneNumber" value="{{$ad->phoneNumber}}" class="form-control">
-                                        <button type="submit" name="update" value="update" class="btn btn-success">ذخیره تغییرات</button>
-                                    </td>
-                                </form>
+                                    @csrf
+                                    <!-- <input type="hidden" name="adIDforCRUD" class="adID" value="{{$ad->id}}"> -->
+                                    <input type="text" name="newAdTitle" value="{{$ad->title}}" class="form-control">
+                                    <input type="text" name="newAdDescription" value="{{$ad->description}}" class="form-control">
+                                    <input type="text" name="newAdAdPrice" value="{{$ad->price}}" class="form-control">
+                                    <input type="text" name="newAdAddress" value="{{$ad->address}}" class="form-control">
+                                    <input type="text" name="newAdPhoneNumber" value="{{$ad->phoneNumber}}" class="form-control">
+                            <td class="align-middle">
+                                <button type="submit" name="update" value="update" class="btn btn-success"><img src="/images/Oxygen-Icons.org-Oxygen-Actions-document-edit.ico" alt="ویرایش" style="height: 30px;"></button>
+                            </td>
+                            </form>
+                            </td>
+                            <td class="align-middle">
                                 <form action="{{$ad->id}}/destroy" method="post">
-                                @csrf
-                                    <td>
-                                        <button type="submit" name="delete" value="delete" class="btn btn-danger">delete</button>
-                                    </td>
+                                    @csrf
+                                    <button type="submit" name="delete" value="delete" class="btn btn-danger"><img src="/images/Iconshock-Vista-General-Trash.ico" alt="پاک پاک" style="height: 30px;"></button>
                                 </form>
-                            </div>
+                            </td>
                         </tr>
 
                         @endforeach
                     </table>
 
-                    <a class="page-link d-flex justify-content-center border m-5 text-success bg-dark" href='{{$url = route("create")}}'>آگهی جدید ایجاد کنید</a>
+                    <a class=" font-weight-bold page-link justify-content-center border text-danger bg-dark h3" href='{{$url = route("create")}}'>آگهی جدید ایجاد کنید</a>
                 </div>
             </div>
         </div>
