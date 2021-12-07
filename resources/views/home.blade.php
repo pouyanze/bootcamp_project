cc8172c254128215c588c20b3abd1d4660adb720
ian <ian@ian.com>
2021-12-08 00:18:53 +0330

    changed for all ads in one place

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+        <div class="text-center">
+            <div>
+                <div > 
+                    <h1>تمام آگهی ها</h1>
+                    <hr>
+                    @foreach ($ads as $ad)
+                    <div class="row justify-content-around col-md-12">
+                        <table class="table table-bordered table-hover">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th scope="col" class="col-3">عنوان</th>
+                                    <th scope="col" class="col-4">توضیحات</th>
+                                    <th scope="col" class="col-1">قیمت</th>
+                                    <th scope="col" class="col-3">آدرس</th>
+                                    <th scope="col" class="col-1">شماره تماس</th>
+                                </tr>
+                            </thead>
+                            <tr>
+                                <td>
+                                    <span>{{$ad->title}}</span>
+                                </td>
+                                <td>
+                                    <span class="col-4">{{$ad->description}}</span>
+                                </td>
+                                <td>
+                                    <span>{{$ad->price}}</span>
+                                </td>
+                                <td>
+                                    <span>{{$ad->address}}</span>
+                                </td>
+                                <td>
+                                    <span>{{$ad->phoneNumber}}</span>
+                                </td>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                    <hr>
-                    <a href="{{ route('list') }}">لیست آگهی ها</a>
-                    <hr>
-                    <a href="{{ route('create') }}">افزودن آگهی جدید</a>
-                    <hr>
+                            </tr>
+                        </table>
+                    </div>
+                    @endforeach
                 </div>
+                <a class="page-link d-flex justify-content-center border m-5 text-success bg-dark" href='{{$url = route("create")}}'>آگهی جدید ایجاد کنید</a>
             </div>
         </div>
     </div>
 </div>
+</div>
+
 @endsection
