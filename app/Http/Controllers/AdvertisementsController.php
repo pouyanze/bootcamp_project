a711c930c178e8bfb3773ad10c8c9a5cafc71d86
ian <ian@ian.com>
2021-12-19 17:19:00 +0330

    added favourites where necessary

--- app/Http/Controllers/AdvertisementsController.php
+++ app/Http/Controllers/AdvertisementsController.php
@@ -10,8 +10,10 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Advertisement;
 use App\Models\Category;
 use App\Models\Comment;
+use App\Models\Favourite;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\AdsValidationRequest;
+use Illuminate\Support\Facades\Log;
 
 class AdvertisementsController extends Controller
 {
@@ -26,23 +28,70 @@ class AdvertisementsController extends Controller
     public function AllAds()
     {
         $ads = Advertisement::all();
-
-        return view('/home', ['ads' => $ads]);
+        $comments = Comment::all();
+        $favourites = Favourite::all();
+        // $cats= Category::where('id', Advertisement::find('categoryID'))->get();
+        // dd("$cats");
+        return view('/home')
+        ->with('ads', $ads)
+        ->with('favourites', $favourites)
+        ->with('comments', $comments);
+        // ->with('cats', $cats);
     }
+
+    public function AllAdsCategorized($id)
+    {
+        // dd("sss");
+        $adsCategorized = Advertisement::where('categoryID', $id)->get(); 
+        $idsOfAdsCategorized = Advertisement::where('categoryID', $id)->pluck('id')->toArray();
+        // dd($ads);
+        $commentsCategorized = Comment::whereIn('adID',$idsOfAdsCategorized)->get();
+        $catsCategorized = Category::where('id',$id)->get();
+        $favourites = Favourite::all();
+        // dd($commentsCategorized);
+        return view('/homeCategorized')
+            ->with('ads', $adsCategorized)
+            ->with('favourites', $favourites)
+            ->with('comments', $commentsCategorized)
+            ->with('Categories', $catsCategorized);
+    }
+    
     public function list()
     {
+       
         $ads = Advertisement::where('userID', Auth::user()->id)->get();
         $idsOfads=DB::table('advertisements')->where('userID', Auth::user()->id)->pluck('id')->toArray();
-        
+        $categoryIDofAds=DB::table('advertisements')->where('userID', Auth::user()->id)->pluck('categoryID')->toArray();
         // dd($idOfads);
 
         $comments = Comment::whereIn('adID',$idsOfads)->get();
-
+        $cats = Category::whereIn('id',$categoryIDofAds)->get();
         // dd($comments);
-
+        $cats = Category::whereIn('id',$categoryIDofAds)->get();
+        $favourites = Favourite::all();
         return view('ads/list')
             ->with('ads', $ads)
-            ->with('comments', $comments);
+            ->with('comments', $comments)
+            ->with('favourites', $favourites)
+            ->with('cats', $cats);
+    }
+
+    
+    public function categorizedList($id)
+    {
+        dd("ss");
+        $adsCategorized = Advertisement::where('categoryID', $id)->get();     
+        $idsOfAdsCategorized = Advertisement::where('categoryID', $id)->pluck('id')->toArray();
+        // dd($ads);
+        $commentsCategorized = Comment::whereIn('adID',$idsOfAdsCategorized)->get();
+        $catsCategorized = Category::where('id',$id)->get();
+        $favourites = Favourite::all();
+        // dd($commentsCategorized);
+        return view('ads/categorizedList')
+            ->with('ads', $adsCategorized)
+            ->with('comments', $commentsCategorized)
+            ->with('favourites', $favourites)
+            ->with('cats', $catsCategorized);
     }
 
     /**
@@ -53,6 +102,7 @@ class AdvertisementsController extends Controller
     public function create()
     {
         $categories = Category::all();
+
         return view('ads/create', ['categories' => $categories]);
     }
 
@@ -64,6 +114,7 @@ class AdvertisementsController extends Controller
      */
     public function store(AdsValidationRequest $request)
     {
+        
         $ad = Advertisement::create([
             'title' => $request->input('title'),
             'description' => $request->input('description'),
@@ -73,6 +124,9 @@ class AdvertisementsController extends Controller
             'userID' => $request->input('userID'),
             'categoryID' => $request->input('categoryID'),
         ]);
+
+
+        Log::info('saving an ad for user: '. Auth::user()->id);
 
         return redirect('ads/list');
     }
