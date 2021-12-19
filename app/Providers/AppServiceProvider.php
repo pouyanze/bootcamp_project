7a73e886a6d9cf3a3ad2ce9f7eee467080ffb107
ian <ian@ian.com>
2021-12-19 17:21:52 +0330

    this is great! i wanted Categories to be available  to layouts.app and  home and using appserviceprovider is the way!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,17 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Advertisement;
+use App\Models\Category;
+use App\Models\Comment;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\CommentsValidationRequest;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\View;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +34,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer(['layouts.app','/home'], function($view)
+        {
+            $view->with('Categories', Category::orderBy('name')->get());
+        });
     }
 }
