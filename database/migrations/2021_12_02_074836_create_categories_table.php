8409537da5590ea53c546a084fb4888ce701bb59
ian <ian@ian.com>
2021-12-29 13:25:00 +0330

    edit- removed "parentID" from categories

--- database/migrations/2021_12_02_074836_create_categories_table.php
+++ database/migrations/2021_12_02_074836_create_categories_table.php
@@ -17,7 +17,6 @@ class CreateCategoriesTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('nameEn');
-            $table->integer('parentID');
         });
     }
 
