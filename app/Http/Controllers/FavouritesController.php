483569c349a30e6a72e8afb4783595f3b9d14758
ian <ian@ian.com>
2021-12-29 13:16:29 +0330

    update- change return redirect TO return back

--- app/Http/Controllers/FavouritesController.php
+++ app/Http/Controllers/FavouritesController.php
@@ -7,26 +7,7 @@ use Illuminate\Http\Request;
 
 class FavouritesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+   
     /**
      * Store a newly created resource in storage.
      *
@@ -41,18 +22,7 @@ class FavouritesController extends Controller
             ['favourite' => $request->input('favourite')]
         );
 
-        return redirect('/home');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+        return back()->withInput();
     }
 
     /**
@@ -70,7 +40,7 @@ class FavouritesController extends Controller
             'advertisement_id' => $request->input('adID'),
         ]);
 
-        return redirect('/home');
+        return back()->withInput();
     }
 
     /**
@@ -80,19 +50,4 @@ class FavouritesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
