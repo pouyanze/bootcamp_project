6cfcdc8a0d49aff9875c3317a5dd5487015b35b7
ian <ian@ian.com>
2021-12-29 13:29:09 +0330

    update- added favourites and comments

--- resources/views/homeCategorized.blade.php
+++ resources/views/homeCategorized.blade.php
@@ -6,21 +6,22 @@
         <div class="text-center">
             <div>
                 <div>
-                @foreach ($Categories as $Category)
+                    @foreach ($Categories as $Category)
                     <h1>:تمام آگهی های دسته بندی<br> {{$Category->name}}</h1>
-                @endforeach
+                    @endforeach
                     <hr>
                     @foreach ($ads as $ad)
                     <div class="row justify-content-around col-md-12">
                         <table class="table table-bordered table-hover">
                             <thead class="thead-light">
                                 <tr>
-                                    <th scope="col" class="col-3">عنوان</th>
-                                    <th scope="col" class="col-3">توضیحات</th>
+                                    <th scope="col" class="col-2">عنوان</th>
+                                    <th scope="col" class="col-2">توضیحات</th>
                                     <th scope="col" class="col-1">قیمت</th>
                                     <th scope="col" class="col-2">آدرس</th>
                                     <th scope="col" class="col-1">شماره تماس</th>
                                     <th scope="col" class="col-3">دسته بندی</th>
+                                    @auth<th scope="col" class="col-2">مورد علاقه</th>@endauth
                                 </tr>
                             </thead>
                             <tr>
@@ -41,18 +42,121 @@
                                 </td>
                                 <td>
                                     @foreach ($Categories as $Category)
-                                    @if(($Category->id)===($ad->categoryID))
-                                    <form action="/{{$Category->id}}/listCategorized" method="post">
+                                    <span>{{$Category->name}}</span>
+                                    @endforeach
+                                </td>
+                                <!-- -------------------------------------favs -->
+                                @auth
+                                <td>
+                                    <form action="/favourites/store" method="post">
                                         @csrf
-                                        <input type="submit" class="form-control" value="{{$Category->name}}"></input>
+                                        <input id="yes" type="radio" name="favourite" value="yes">
+                                        <label for="yes">بله</label>
+                                        <input id="no" type="radio" name="favourite" value="no">
+                                        <label for="no">خیر</label>
+                                        <input id="newFav" type="hidden" name="userID" value="{{Auth::user()->id}}">
+                                        <input id="newFav" type="hidden" name="adID" value="{{$ad->id}}">
+                                        <input type="submit" value="ارسال">
                                     </form>
+                                    @foreach ($favourites as $favourite)
+                                    @if ((($favourite->advertisement_id)==($ad->id)) and (($favourite->user_id)==(Auth::user()->id)) and (($favourite->favourite)=='yes'))
+                                    <img src="/images/heartF.ico" alt="full heart">
+                                    @elseif ((($favourite->advertisement_id)==($ad->id)) and (($favourite->user_id)==(Auth::user()->id)) and (($favourite->favourite)=='no'))
+                                    <img src="/images/heartE.ico" alt="empty heart">
                                     @endif
                                     @endforeach
                                 </td>
-
+                                @endauth
+                                <!-- --------------------------------------------- -->
                             </tr>
                         </table>
                     </div>
+                    <!-- -----------------------------------------------comments -->
+                    <div class=" container-fluid dropdown w-100 p-3">
+                        <button class=" btn btn-secondary dropdown-toggle" data-flip="false" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            نظرات
+                        </button>
+                        <div class=" dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <table class="  table table-striped table-dark table-bordered table-hover">
+                                @guest
+                                @foreach ($comments->sortBy('userID') as $comment)
+                                @if (($comment->adID)==($ad->id))
+                                <tr>
+                                    <td>
+                                        <table class="table table-striped table-dark table-bordered table-hover">
+                                            <label class="font-weight-bold page-link justify-content-center border text-success bg-dark h5" for="othersComment">نظر</label>
+                                            <tr>
+                                                <td>
+                                                    <span>{{$comment->title}}</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <span>{{$comment->description}}</span>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                @endif
+                                @endforeach
+                                @endguest
+
+                                @auth
+                                @foreach ($comments->sortBy('userID') as $comment)
+                                @if (($comment->adID)==($ad->id))
+                                <tr>
+                                    <td>
+                                        @if((Auth::user()->id)==($comment->userID))
+                                        <form action="/comments/{{$comment->id}}/edit" method="post">
+                                            <label class="font-weight-bold page-link justify-content-center border text-success bg-dark h5" for="<?php echo ($comment->adID); ?>"> (بفرمایید ادیت)نظر شما </label>
+                                            @csrf
+                                            <input id="<?php echo ($comment->adID); ?>" type="text" name="newAdCommentTitle" value="{{$comment->title}}" class="form-control">
+                                            <input id="<?php echo ($comment->adID); ?>" type="text" name="newAdCommentDescription" value="{{$comment->description}}" class="form-control">
+                                            <button type="submit" name="update" value="update" class="btn btn-success">
+                                                <img src="/images/Oxygen-Icons.org-Oxygen-Actions-document-edit.ico" alt="ویرایش" style="height: 30px;">
+                                            </button>
+                                        </form>
+                                        @else
+                                        <table>
+                                            <label class="font-weight-bold page-link justify-content-center border text-success bg-dark h5" for="othersComment">نظرات دیگران</label>
+                                            <tr>
+                                                <td>
+                                                    <p id="othersComment">{{$comment->title}}</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <p id="othersComment">{{$comment->description}}</p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        @endif
+                                    </td>
+                                </tr>
+                                @endif
+                                @endforeach
+                                <table class=" table table-striped table-dark table-bordered table-hover table-info ">
+                                    <td>
+                                        <label class="font-weight-bold page-link justify-content-center border text-success bg-dark h5" for="newComment">نظر جدید</label>
+                                        <form action="/comments/store2" method="post">
+                                            @csrf
+                                            <input id="newComment" type="text" name="title" class="form-control" placeholder="New Commnet Title">
+                                            <input id="newComment" type="text" name="description" class="form-control" placeholder="New Commnet Text">
+                                            <input id="newComment" type="hidden" name="userID" value="{{Auth::user()->id}}" class="form-control">
+                                            <input id="newComment" type="hidden" name="adID" value="{{$ad->id}}" class="form-control">
+                                            <button type="submit" name="store" value="update" class="btn btn-success">
+                                                <img src="/images/Oxygen-Icons.org-Oxygen-Actions-document-edit.ico" alt="ویرایش" style="height: 30px;">
+                                            </button>
+                                        </form>
+                                    </td>
+                                </table>
+                                @endauth
+                            </table>
+                            <hr>
+                        </div>
+                    </div>
+                    <!-- ----------------------------------------------- -->
                     @endforeach
                 </div>
                 <a class="font-weight-bold page-link justify-content-center border text-danger bg-dark h3" href='{{$url = route("create")}}'>آگهی جدید ایجاد کنید</a>
@@ -60,6 +164,6 @@
         </div>
     </div>
 </div>
-</div>
+
 
 @endsection
