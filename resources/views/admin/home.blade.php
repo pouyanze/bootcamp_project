2f7c8524c413c68b61a5932829a46223a2e27e6d
ian <ian@ian.com>
2021-12-29 13:33:33 +0330

    create - home page for admin

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.adminDashboard')
+@section('content')
+
+<div class="d-flex align-items-center justify-content-center" style="height: 350px">
+    <h1 class="my-auto mx-auto">صفحه مدیر سایت</h1>
+</div>
+
+@endsection
