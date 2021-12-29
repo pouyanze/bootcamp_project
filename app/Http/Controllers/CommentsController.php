c1bc58a4beb808277000d75343c66f769e5f171a
ian <ian@ian.com>
2021-12-29 13:16:01 +0330

    update- change return redirect TO return back()

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -17,25 +17,6 @@ use Illuminate\Support\Facades\Log;
 
 class CommentsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -53,9 +34,9 @@ class CommentsController extends Controller
             'adID' => $request->input('adID'),
         ]);
 
-        Log::info('saving a comment for user: '. Auth::user()->id);
+        Log::info('saving a comment by user: '. Auth::user()->id);
 
-        return redirect('ads/list');
+        return back()->withInput();
     }
     public function store2(Request $request)
     {
@@ -69,20 +50,10 @@ class CommentsController extends Controller
 
         Log::info('saving a comment for user: '. Auth::user()->id);
 
-        return redirect('/home');
+        return back()->withInput();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
+   
     /**
      * Show the form for editing the specified resource.
      *
@@ -95,29 +66,7 @@ class CommentsController extends Controller
             'title' => $request->input('newAdCommentTitle'),
             'description' => $request->input('newAdCommentDescription'),
         ]);
-        return redirect('ads/list');
+        return back()->withInput();
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
